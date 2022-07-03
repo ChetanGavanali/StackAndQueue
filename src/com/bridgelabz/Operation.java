ac65c8681155a36b007b1526b7d359bda5b175ec
ChetanGavanali <chetangavanali@gmail.com>
2022-07-03 12:35:01 +0530

    Added: CreateStack

---
+++ src/com/bridgelabz/Operation.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class Operation {
+    public static StackAndQueue addElement() {
+        StackAndQueue stacklist = new StackAndQueue();
+        stacklist.push(70);
+        stacklist.push(30);
+        stacklist.push(56);
+
+        stacklist.display();
+        return stacklist;
+
+    }
+}
+
